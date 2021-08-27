9dfc593fa90730547664e495caa47fc75f7c126a
Timo Scheuermann <timo.scheuermann@gmx.de>
2021-08-27 18:24:11 +0200

    updated build

--- src/lib-components/index.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-
-export { default as VcFastBackward } from './vcFastBackward.vue';
-export { default as VcFastForward } from './vcFastForward.vue';
-export { default as VcPause } from './vcPause.vue';
-export { default as VcPlay } from './vcPlay.vue';
-export { default as VcStepBackwards } from './vcStepBackwards.vue';
-export { default as VcStepForward } from './vcStepForward.vue';
-export { default as VcStop } from './vcStop.vue';
